0f481834635d573b078dcca62354d0b6695bfb41
Aleksandar <sleasarbajic@gmail.com>
2023-10-02 09:56:54 +0200

    Updated the Data

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -7,6 +7,7 @@ import {
   HiOutlineHomeModern,
   HiOutlineUsers,
 } from "react-icons/hi2";
+import Uploader from "../data/Uploader";
 
 const NavList = styled.ul`
   display: flex;
@@ -86,6 +87,7 @@ function MainNav() {
             <span>Settings</span>
           </StyledNavLink>
         </li>
+        {/* <Uploader /> */}
       </NavList>
     </nav>
   );
