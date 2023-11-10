c2c1ec77bf2cc9c46efdd3854143811ffd5b7455
Aleksandar <sleasarbajic@gmail.com>
2023-11-10 22:45:03 +0100

    The Wild Oasis

--- src/features/checkinout/CheckinBooking.jsx
+++ src/features/checkinout/CheckinBooking.jsx
@@ -17,7 +17,6 @@ import { useCheckin } from "./useCheckin";
 import { useSettings } from "../settings/useSettings";
 
 const Box = styled.div`
-  /* Box */
   background-color: var(--color-grey-0);
   border: 1px solid var(--color-grey-100);
   border-radius: var(--border-radius-md);
